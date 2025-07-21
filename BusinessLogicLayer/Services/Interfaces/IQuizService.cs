a3fc964fff104fe36b9a1b4ee9852e3568f4a572
LTP <lamphat279@gmail.com>
2025-07-21 23:08:18 +0700

    feat : done SOLID

--- BusinessLogicLayer/Services/Interfaces/IQuizService.cs
+++ BusinessLogicLayer/Services/Interfaces/IQuizService.cs
@@ -1,5 +1,8 @@
 using DataAccessLayer.Models.ViewModels;
 using BusinessLogicLayer.DTOs.Common;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Security.Claims;
+using BusinessLogicLayer.Services.Implementations;
 
 namespace BusinessLogicLayer.Services.Interfaces
 {
@@ -15,6 +18,18 @@ namespace BusinessLogicLayer.Services.Interfaces
         Task<ServiceResult<bool>> UpdateQuizStatusAsync(string quizId, string userId, int newStatus);
         Task<ServiceResult<QuizStatisticsViewModel>> GetQuizStatisticsAsync(string quizId, string userId);
         Task CleanupAbandonedAttemptsAsync();
+        // Controller-facing methods
+        Task<CreateQuizResult> GetCreateQuizAsync(ClaimsPrincipal user, string chapterId);
+        Task<CreateQuizResult> CreateQuizAsync(ClaimsPrincipal user, CreateQuizViewModel model, ModelStateDictionary modelState);
+        Task<EditQuizResult> GetEditQuizAsync(ClaimsPrincipal user, string quizId);
+        Task<EditQuizResult> UpdateQuizAsync(ClaimsPrincipal user, CreateQuizViewModel model, ModelStateDictionary modelState);
+        Task<DeleteQuizResult> DeleteQuizAsync(ClaimsPrincipal user, string quizId);
+        Task<QuizDetailsResult> GetQuizDetailsAsync(ClaimsPrincipal user, string quizId);
+        Task<QuizPreviewResult> GetQuizPreviewAsync(ClaimsPrincipal user, string quizId);
+        Task<QuizTakeResult> GetQuizTakeAsync(ClaimsPrincipal user, string quizId);
+        Task<QuizSubmitResult> SubmitQuizAsync(ClaimsPrincipal user, QuizTakeSubmitViewModel model, ModelStateDictionary modelState);
+        Task<QuizResultResult> GetQuizResultAsync(ClaimsPrincipal user, string attemptId);
+        Task<GetQuizQuestionsResult> GetQuizQuestionsAsync(ClaimsPrincipal user, string quizId);
     }
 }
 
